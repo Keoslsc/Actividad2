b766ea38b3ae74b0a528711815b965482ba05cbb
Carlos López <carlosadrian1103@gmail.com>
2020-09-20 21:27:19 -0500

    Se crea un programa en go para calcular el área de un cuadrado con entradas del usuario

---
+++ cuadrado.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var lado float32
+	var area float32
+
+	fmt.Println("Calcular área de un cuadrado.")
+	fmt.Println("-----------------------------")
+	fmt.Printf("Digite el valor del lado:")
+	fmt.Scan(&lado)
+	area = lado * lado
+	fmt.Printf("El area del cuadrado es: %.2f.", area)
+}
